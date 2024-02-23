14b91392eccf8c56176caf7fea11f0debdb0ce29
imc836 <159478164+imc836@users.noreply.github.com>
2024-02-23 18:45:34 +0000

    saludo

---
+++ Practica 1/saludo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* Programa inicial */
+
+int main(){
+    printf("Hola.\n");
+    printf("Este es mi primer programa\n");
+    printf("escrito en C de la carrera.\n");
+    printf("Adios.\n");
+    printf("\n\n\nPulse una tecla para finalizar");
+    getchar();
+    return 0;
+}
